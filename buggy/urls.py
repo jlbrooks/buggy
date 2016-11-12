ac08f235e7aec5fba340b46af9f284d281d5a9c1
Jacob Brooks <jacob@chrisbrooks.org>
2016-11-12 10:45:38 -0500

    Bootstrap, start of pusher form

--- buggy/urls.py
+++ buggy/urls.py
@@ -19,5 +19,6 @@ from pushers import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index)
+    url(r'^$', views.index, name='index'),
+    url(r'^create_pusher$', views.create_pusher, name='create_pusher'),
 ]
--- pushers/views.py
+++ pushers/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def create_pusher(request):
+    return index(request)
