7a80b1bb4e280412c8c8d71c1475036d5241cd71
Jacob Brooks <jacob@chrisbrooks.org>
2016-11-14 15:13:22 -0500

    Assigning 2-5's

--- pushers/models.py
+++ pushers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 import random
 
 class Pusher(models.Model):
@@ -41,19 +42,42 @@ class Roll(models.Model):
         pushers = self.pushers
         # Shuffle the list of pushers
         random.shuffle(pushers)
-        cur_pusher = 0
-        
-        for buggy in buggies:
-            bRoll,_ = BuggyRoll.objects.get_or_create(buggy=buggy, roll=self)
-            bRoll.save()
+        # First, generate/retrieve the rolls for each buggy
+        bRolls = [BuggyRoll.objects.get_or_create(buggy=b, roll=self)[0] for b in buggies]
+        # Build all of the hill objects
+        hills = []
+        for bRoll in bRolls:
             for i in range(2,6):
-                hill,_ = RollHill.objects.get_or_create(roll=bRoll,hill=i,pusher=pushers[cur_pusher])
-                cur_pusher = cur_pusher + 1
-                # If we wrap, re-shuffle and start over
-                if cur_pusher >= len(pushers):
-                    random.shuffle(pushers)
-                    cur_pusher = 0
-                hill.save()
+                try:
+                    hill = RollHill.objects.get(roll=bRoll,hill=i)
+                except RollHill.DoesNotExist:
+                    hill= RollHill(roll=bRoll,hill=i)
+                hills.append(hill)
+
+        # If we can't do it with 2-5's then don't try
+        if len(pushers) + len(buggies) < len(hills):
+            return "Not enough pushers - need %d" % len(hills) - len(buggies)
+
+        # Assign 2-5's
+        while len(pushers) < len(hills):
+            hill_2 = [h for h in hills if h.hill == 2][0]
+            hill_5 = [h for h in hills if h.hill == 5][0]
+            pusher = pushers.pop()
+            hill_2.pusher = pusher
+            hill_5.pusher = pusher
+            hills.remove(hill_2)
+            hills.remove(hill_5)
+
+            hill_2.save()
+            hill_5.save()
+        
+        # Assign rest of pushers to hills 
+        while len(hills) > 0:
+            hill = hills.pop()
+            hill.pusher = pushers.pop()
+            hill.save()
+
+        return None
 
 class BuggyRoll(models.Model):
     buggy = models.ForeignKey(Buggy)
