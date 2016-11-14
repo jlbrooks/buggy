46e5ea7e035fb33b661c3363df68d3512e46b9cd
Jacob Brooks <jacob@chrisbrooks.org>
2016-11-14 17:24:29 -0500

    Local settings

--- buggy/settings.py
+++ buggy/settings.py
@@ -75,16 +75,17 @@ WSGI_APPLICATION = 'buggy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {'default': {}}
-DATABASES['default'].update(db_from_env)
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+if 'HEROKU' in os.environ:
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES = {'default': {}}
+    DATABASES['default'].update(db_from_env)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
